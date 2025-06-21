a3ca90c6ba734d67fd34b9e578c1074d55ca953a
Paul PLANCQ <paul.plancq@outlook.fr>
2025-06-21 21:40:59 +0200

    feat: add LiskovSubstitutionSlide component and update slides export

--- src/Slides/index.ts
+++ src/Slides/index.ts
@@ -3,6 +3,7 @@ import { IntroductionCleanArchitectureSlide } from '@Front/Slides/02_Introductio
 import { FirstStepSlide } from '@Front/Slides/03_FirstStep';
 import { SingleResponsibilitySlide } from '@Front/Slides/04_SingleResponsibility';
 import { OpenCloseSlide } from '@Front/Slides/05_OpenClose';
+import { LiskovSubstitutionSlide } from '@Front/Slides/06_LiskovSubstitution';
 import { type ComponentType } from 'react';
 
 export const slides = [
@@ -11,4 +12,5 @@ export const slides = [
   FirstStepSlide,
   SingleResponsibilitySlide,
   OpenCloseSlide,
+  LiskovSubstitutionSlide,
 ] as ComponentType[];
